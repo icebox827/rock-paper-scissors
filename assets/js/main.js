215c758346dbffd7de259555ca10b7c1f967e4ff
Denis Lafontant <icebox827@gmail.com>
2021-07-31 14:53:03 -0400

    Create playGame function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,7 @@ function computerPlay() {
   const random = ['rock', 'paper', 'scissors'];
   return random[Math.floor(Math.random() * 3)];
 }
+
+function playGame(playerSelection, computerSelection) {
+  
+}
