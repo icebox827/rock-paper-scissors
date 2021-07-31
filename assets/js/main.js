496d3d71a27f6b174015bda3fb539af7e3badfaf
Denis Lafontant <icebox827@gmail.com>
2021-07-31 15:50:03 -0400

    Create playerSelection function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,20 @@
-function computerPlay() {
-  const random = ['rock', 'paper', 'scissors'];
-  return random[Math.floor(Math.random() * 3)];
+function computerPlay () {
+  const random = ['rock', 'paper', 'scissors']
+  return random[Math.floor(Math.random() * 3)]
 }
 
-function playRound(playerSelection, computerSelection) {
+const playerSelection = userInput => {
+  userInput = userInput.toLowerCase()
+  if (userInput == 'rock' || userInput == 'paper' || userInput == 'scissors') {
+    return userInput
+  } else {
+    return 'Invalid choice! Please try again'
+  }
+}
+
+function playRound (playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return 'Tie! Try again';
+    return 'Tie! Try again'
   } else {
     return 'You lose! Nice try'
   }
